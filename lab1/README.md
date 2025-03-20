bada731df05173f7de23ec9b1708bcb6be682a8f
marcos <marcospiccotto12@gmail.com>
2025-03-19 21:19:41 -0300

    add: Docstrings for main

--- lab1/README.md
+++ lab1/README.md
@@ -1,20 +1,28 @@
 # Cosas a consultar en clase
 
-preguntar si siempre el id va a ser +1 en de la pos del arreglo o es cuestion de diseño
+- preguntar si siempre el id va a ser +1 en de la pos del arreglo o es cuestion de diseño
+- se puede editar el id de una pelicula ??
+- se puede editar solo un campo una pelicula o tiene que ser todo completo ?
+- en agregar peliculas deberiamos obligar que llene todos los campos ?
 
 ------
 
 # Cosas que tenemos que hacer
 
-agregar docstrings en todas las funciones
-revisar si faltan revisar index o condiciones
+- agregar docstrings en todas las funciones
+- revisar si faltan revisar index o condiciones
+- update esta bien ?
 
 ------
 
 # Cosas que tenemos que charlar
 
 - estan bien todas las respuestas de las rutas ?
+- en agregar peliculas deberiamos obligar que llene todos los campos ?
 
+# Cosas que poner en el readme
+- queriamos que los codigo de http sean algunos otros como el 204 y demas, pero lo cambiamos por los tests
+- por lo visto la funcion actualizar debe devolver lo que actualizaste
 
 ## envir peticiones
 ### postman
--- lab1/main.py
+++ lab1/main.py
@@ -16,24 +16,64 @@ peliculas = [
     {'id': 12, 'titulo': 'Fight Club', 'genero': 'Drama'}
 ]
 
-def is_valid_id(id):
+def id_valido(id):
+    """
+    Verfica que un id este en rango valido
+
+    Retorna:
+    True si esta en rango valido
+    False si no esta en rango valido
+    """
     return id < 1 or id > len(peliculas)  
 
 
 def obtener_peliculas():
+    """
+    Obtiene la lista completa de películas disponibles.
+
+    Retorna:
+    Un objeto JSON con la lista de películas.
+    """
     return jsonify(peliculas)
 
 
 def obtener_pelicula(id):
-    # Lógica para buscar la película por su ID y devolver sus detalles
-    if(is_valid_id(id)):
-        return jsonify({"error": "Invalid movie ID"}), 400
+    """
+    Busca una película por su ID y devuelve sus detalles en formato JSON.
+
+    Parámetros:
+    id (int): Identificador de la película a buscar.
+
+    Assert: 
+    comprobamos si id esta en rango valido (0 < id < len peliculas).
+
+    Retorna: 
+    200 si la película se agregó correctamente.
+    400 si el id es invalido.
+    """
+
+    if(id_valido(id)):
+        return jsonify({"Error": "ID de película inválido"}), 400
     
     pelicula_encontrada = peliculas[id-1]
-    return jsonify(pelicula_encontrada)
+    return jsonify(pelicula_encontrada), 200
 
 
 def agregar_pelicula():
+    """
+    Agrega una nueva película a la lista.
+
+    Assert: recibir un JSON con los campos 'nombre' y 'genero'.
+
+    Retorna:
+    201 si la película se agregó correctamente, junto con la película agregada.
+    400 si faltan parámetros obligatorios.
+    """
+
+    campos_pelicula_nueva = request.json.keys()
+    if 'titulo' not in campos_pelicula_nueva or 'genero' not in campos_pelicula_nueva:
+        return jsonify({"Error": "ID de película inválido"}), 400
+    
     nueva_pelicula = {
         'id': obtener_nuevo_id(),
         'titulo': request.json['titulo'],
@@ -44,34 +84,66 @@ def agregar_pelicula():
 
 
 def actualizar_pelicula(id):
-    # Lógica para buscar la película por su ID y actualizar sus detalles
-    if is_valid_id(id):
-        return jsonify({"error": "Invalid movie ID"}), 400
+    """
+    Actualiza una película por su ID y devuelve sus detalles en formato JSON.
+
+    Parámetros:
+    id (int): Identificador de la película a buscar.
+
+    Assert: 
+    comprobamos si id esta en rango valido (0 < id < len peliculas).
+
+    Retorna: 
+    200 si la película se agregó correctamente.
+    400 si el id es invalido o se intenta modificar el id.
+    """
+    
+    if id_valido(id):
+        return jsonify({"error": "ID de película inválido"}), 400
     
     nueva_pelicula = request.json
     if "id" in nueva_pelicula.keys():
-        return jsonify({"error": "ID modification is not allowed"}), 400
+        return jsonify({"error": "No esta permitido editar el ID"}), 400
     
-    peliculas[id-1] = {
+    nueva_pelicula = {
         "id":id,
         **nueva_pelicula
     }
-    return '', 204
+    peliculas[id-1] = nueva_pelicula
+    return jsonify(nueva_pelicula), 200
 
 
 def eliminar_pelicula(id):
-    # Lógica para buscar la película por su ID y eliminarla
+    """
+    Elimina una película por su ID.
 
-    if is_valid_id(id):
-        return jsonify({"error": "Invalid movie ID"}), 400
+    Parámetros:
+    id (int): Identificador de la película a buscar.
+
+    Assert: 
+    comprobamos si id esta en rango valido (0 < id < len peliculas).
+
+    Retorna: 
+    200 si la película se agregó correctamente.
+    400 si el id es invalido o se intenta modificar el id.
+    """
+
+    if id_valido(id):
+        return jsonify({"error": "ID de película inválido"}), 400
     
     del peliculas[id-1]
     for i in range(id-1, len(peliculas)):
         peliculas[i]["id"] -= 1 
-    return '', 204
+    return '', 200
 
 
 def obtener_nuevo_id():
+    """
+    Obtener un nuevo id.
+
+    Retorna: 
+    Un id (int).
+    """
     if len(peliculas) > 0:
         ultimo_id = peliculas[-1]['id']
         return ultimo_id + 1
